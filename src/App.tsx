13b50e3ad74e5ba29cdeb6adf6367b0170c5fdf7
Severyn Vehera <severyn.vehera@shamaym.com>
2023-08-10 18:43:06 +0300

    make changes

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const App = () => {
         { make: 'Toyota', model: 'Celica', price: 35000 },
         { make: 'Ford', model: 'Mondeo', price: 32000 },
         { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
         { make: 'Porsche', model: 'Boxster', price: 72000 }
     ]);
 
@@ -33,6 +34,7 @@ const App = () => {
                 </AgGridReact>
             </div>
         </div>
+        
     );
 };
 
