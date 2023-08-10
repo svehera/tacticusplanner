4b6935a41697dcd04c623da3d3a5124be0b7d0e4
Severyn Vehera <severyn.vehera@shamaym.com>
2023-08-10 18:05:21 +0300

    add es lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-material.css';
-import {AgGridReact} from "ag-grid-react";
-import {ColDef} from "ag-grid-community";
+import { AgGridReact } from 'ag-grid-react';
+import { ColDef } from 'ag-grid-community';
 
 
 function App() {
     
     const [rowData] = useState([
-        {make: "Toyota", model: "Celica", price: 35000},
-        {make: "Ford", model: "Mondeo", price: 32000},
-        {make: "Porsche", model: "Boxster", price: 72000}
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxster', price: 72000 }
     ]);
 
     const [columnDefs] = useState<Array<ColDef>>([
@@ -22,7 +23,7 @@ function App() {
     ]);
 
     return (
-        <div className="ag-theme-material" style={{height: 400, width: 600}}>
+        <div className="ag-theme-material" style={{ height: 400, width: 600 }}>
             <AgGridReact
                 rowData={rowData}
                 columnDefs={columnDefs}>
