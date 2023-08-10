ba72d6e51ac5ef66284666aa65ac88347fb067d0
Severyn Vehera <severyn.vehera@shamaym.com>
2023-08-10 17:59:33 +0300

    add ag grid package

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
+import {AgGridReact} from "ag-grid-react";
+import {ColDef} from "ag-grid-community";
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Tacticus Planner
-        </a>
-      </header>
-    </div>
-  );
+    
+    const [rowData] = useState([
+        {make: "Toyota", model: "Celica", price: 35000},
+        {make: "Ford", model: "Mondeo", price: 32000},
+        {make: "Porsche", model: "Boxster", price: 72000}
+    ]);
+
+    const [columnDefs] = useState<Array<ColDef>>([
+        { field: 'make' },
+        { field: 'model' },
+        { field: 'price' }
+    ]);
+
+    return (
+        <div className="ag-theme-material" style={{height: 400, width: 600}}>
+            <AgGridReact
+                rowData={rowData}
+                columnDefs={columnDefs}>
+            </AgGridReact>
+        </div>
+    );
 }
 
 export default App;
