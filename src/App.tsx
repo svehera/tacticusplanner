0c5703a797f851d7de9aa51438969e913909aad7
Severyn Vehera <severyn.vehera@shamaym.com>
2023-08-10 17:40:30 +0300

    Update title

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tacticus Planner
         </a>
       </header>
     </div>
