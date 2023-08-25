898734535ded6a2bc1d8522e239e396c1e1f5be4
Severyn Vehera <severyn.vehera@shamaym.com>
2023-08-25 11:51:33 +0300

    update about.tsx

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -80,8 +80,8 @@ const About = () => {
             <h4>What are future plans for the Tacticus Planner?</h4>
             <p>UI/UX improvements</p>
             <p>Ukrainian localization</p>
-            <p>&quot;Stats&quot; feature - table with characters stats (range/melee hits, range distance, movements,
-                etc.)</p>
+            <p>Legendary Events auto-suggested teams</p>
+            <p>Google Drive integration</p>
             <p>More Filters - Nandi&apos;s &quot;Dirty Dozed&quot;, Nandi Recommends</p>
             <p>&quot;My Lists&quot; feature - create your own PvP, Guild raids lists.</p>
             <p>More personal data on Who You Own page - rarity, abilities level, power</p>
