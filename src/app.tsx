bfc35291d37fb410415d69b5af7940c8e65b315f
Severyn Vehera <severyn.vehera@shamaym.com>
2023-09-14 12:56:20 +0300

    add redirect from 404 page

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate, useSearchParams } from 'react-router-dom';
 
 import TopAppBar from './app-bar';
 import { GlobalService } from './services';
@@ -7,13 +7,18 @@ import { isMobile } from 'react-device-detect';
 
 const App = () => {
     const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
 
     useEffect(() => {
         if (isMobile) {
             navigate('/mobile');
         }
+        const redirect = searchParams.get('redirect');
+        if(redirect) {
+            navigate(redirect);
+        }
     }, []);
-
+    
     return (
         <div style={{ width: '100%', height: '100%' }}>
             <TopAppBar></TopAppBar>
