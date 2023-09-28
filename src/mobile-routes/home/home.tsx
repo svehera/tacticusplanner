4b132357aaa76c17d6aa9cd076ba70f628a91611
Severyn Vehera <severyn.vehera@shamaym.com>
2023-09-28 17:30:11 +0300

    update discord link

--- src/mobile-routes/home/home.tsx
+++ src/mobile-routes/home/home.tsx
@@ -127,7 +127,7 @@ export const Home = () => {
                     <AccordionDetails>
                         <p>Send email to <a href="mailto: tacticusplanner@gmail.com" target={'_blank'}
                             rel="noreferrer">tacticusplanner@gmail.com</a> or reach me out in <a
-                            href="https://discord.gg/daK7y6f8" target={'_blank'} rel="noreferrer">Discord</a></p>
+                            href="https://discord.gg/B2ze6w7gx" target={'_blank'} rel="noreferrer">Discord</a></p>
                     </AccordionDetails>
                 </Accordion>
 
--- src/routes/root/about.tsx
+++ src/routes/root/about.tsx
@@ -119,7 +119,7 @@ export const About = () => {
                     <AccordionDetails>
                         <p>Send email to <a href="mailto: tacticusplanner@gmail.com" target={'_blank'}
                             rel="noreferrer">tacticusplanner@gmail.com</a> or reach me out in <a
-                            href="https://discord.gg/daK7y6f8" target={'_blank'} rel="noreferrer">Discord</a></p>
+                            href="https://discord.gg/B2ze6w7gx" target={'_blank'} rel="noreferrer">Discord</a></p>
                     </AccordionDetails>
                 </Accordion>
     
