ff40b2bfbb43e0d8d3ae6f5da95a4d6af9a2c75b
Severyn Vehera <severyn.vehera@shamaym.com>
2023-10-17 22:02:44 +0300

    extract discord invitation link

--- src/routes/contacts/contacts.tsx
+++ src/routes/contacts/contacts.tsx
@@ -1,13 +1,19 @@
 ﻿import React from 'react';
+import { discordInvitationLink } from '../../models/constants';
 
 export const Contacts = () => {
-
     return (
         <div>
-            <p>Send email to <a href="mailto: tacticusplanner@gmail.com" target={'_blank'}
-                rel="noreferrer">tacticusplanner@gmail.com</a> or reach me out in <a
-                href="https://discord.gg/B2ze6w7gx" target={'_blank'} rel="noreferrer">Discord</a></p>
+            <p>
+                Send email to{' '}
+                <a href="mailto: tacticusplanner@gmail.com" target={'_blank'} rel="noreferrer">
+                    tacticusplanner@gmail.com
+                </a>{' '}
+                or reach me out in{' '}
+                <a href={discordInvitationLink} target={'_blank'} rel="noreferrer">
+                    Discord
+                </a>
+            </p>
         </div>
     );
 };
-
