585f1b8e6577cbdc214d19d20ea754a24e393614
Severyn Vehera <severyn.vehera@shamaym.com>
2024-02-01 14:55:30 +0200

    notify about bundle error

--- src/app-routing.tsx
+++ src/app-routing.tsx
@@ -1,4 +1,4 @@
-﻿import { createBrowserRouter } from 'react-router-dom';
+﻿import { createBrowserRouter, useRouteError } from 'react-router-dom';
 import { appRoutes } from './routes/routes';
 import { mobileAppRoutes } from './mobile-routes/routes';
 import React from 'react';
@@ -7,6 +7,19 @@ import { App } from './app';
 export const routes = createBrowserRouter([
     {
         path: '',
+        ErrorBoundary() {
+            const error = useRouteError() as Error;
+            if (!error.message.includes('dynamically imported module')) {
+                throw error;
+            }
+            return (
+                <>
+                    <h1>Uh oh!</h1>
+                    <p>Something went wrong! Probably the app was updated.</p>
+                    <button onClick={() => window.location.reload()}>Click here to reload the page</button>
+                </>
+            );
+        },
         element: <App />,
         children: [...appRoutes(), ...mobileAppRoutes()],
     },
