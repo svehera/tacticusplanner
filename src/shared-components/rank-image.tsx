e29c9c62398b95fcab55b5c33a8274e64bcfe072
Severyn Vehera <severyn.vehera@shamaym.com>
2024-03-10 15:14:55 +0200

    small updates

--- src/shared-components/rank-image.tsx
+++ src/shared-components/rank-image.tsx
@@ -1,5 +1,4 @@
 ﻿import { Rank } from '../models/enums';
-import { Tooltip } from '@mui/material';
 import React from 'react';
 import { getImageUrl } from '../shared-logic/functions';
 
@@ -11,11 +10,5 @@ export const RankImage = ({ rank }: { rank: Rank }) => {
     }
 
     const image = getImageUrl(`ranks/${rankTextValue.toLowerCase()}.png`);
-    return (
-        <Tooltip title={rankTextValue} leaveDelay={1000}>
-            <span style={{ height: 30 }}>
-                <img loading={'lazy'} style={{ pointerEvents: 'none' }} src={image} height={30} alt={rankTextValue} />
-            </span>
-        </Tooltip>
-    );
+    return <img loading={'lazy'} style={{ pointerEvents: 'none' }} src={image} height={30} alt={rankTextValue} />;
 };
