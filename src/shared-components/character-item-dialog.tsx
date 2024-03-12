412e62f31b12fc784534a50c3dc3005bbb0d9156
Severyn Vehera <severyn.vehera@shamaym.com>
2024-03-12 21:10:48 +0200

    respect view settings on edit character dialog

--- src/shared-components/character-item-dialog.tsx
+++ src/shared-components/character-item-dialog.tsx
@@ -12,10 +12,12 @@ import { CharactersValueService } from 'src/v2/features/characters/characters-va
 import { ICharacter2, IMaterialRecipeIngredientFull } from '../models/interfaces';
 import { CharacterTitle } from './character-title';
 import { CharacterDetails } from '../mobile-routes/characters/character-details';
-import { DispatchContext } from '../reducers/store.provider';
+import { DispatchContext, StoreContext } from '../reducers/store.provider';
 import { MiscIcon } from './misc-icon';
+import { Conditional } from 'src/v2/components/conditional';
 
 export const CharacterItemDialog = (props: { character: ICharacter2; isOpen: boolean; onClose: () => void }) => {
+    const { viewPreferences } = useContext(StoreContext);
     const [character, setCharacter] = useState(() => ({ ...props.character }));
     const [inventoryUpdate, setInventoryUpdate] = useState<IMaterialRecipeIngredientFull[]>([]);
 
@@ -41,14 +43,18 @@ export const CharacterItemDialog = (props: { character: ICharacter2; isOpen: boo
                         justifyContent: 'space-between',
                     }}>
                     <CharacterTitle character={character} />
-                    <div style={{ display: 'flex' }}>
-                        <MiscIcon icon={'blackstone'} height={20} width={15} />{' '}
-                        {CharactersValueService.getCharacterValue(character).toLocaleString().replace(/,/g, ' ')}
-                    </div>
-                    <div style={{ display: 'flex' }}>
-                        <MiscIcon icon={'power'} height={20} width={15} />{' '}
-                        {CharactersPowerService.getCharacterPower(character).toLocaleString().replace(/,/g, ' ')}
-                    </div>
+                    <Conditional condition={viewPreferences.showBsValue}>
+                        <div style={{ display: 'flex' }}>
+                            <MiscIcon icon={'blackstone'} height={20} width={15} />{' '}
+                            {CharactersValueService.getCharacterValue(character).toLocaleString().replace(/,/g, ' ')}
+                        </div>
+                    </Conditional>
+                    <Conditional condition={viewPreferences.showPower}>
+                        <div style={{ display: 'flex' }}>
+                            <MiscIcon icon={'power'} height={20} width={15} />{' '}
+                            {CharactersPowerService.getCharacterPower(character).toLocaleString().replace(/,/g, ' ')}
+                        </div>
+                    </Conditional>
                 </div>
             </DialogTitle>
             <DialogContent style={{ paddingTop: 20 }}>
