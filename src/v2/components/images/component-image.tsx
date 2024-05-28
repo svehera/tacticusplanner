5483f657c7a620eb24d7533c1b935594ba62ae2d
Severyn Vehera <severyn.vehera@shamaym.com>
2024-05-28 18:17:33 +0300

    update path

--- src/v2/components/images/component-image.tsx
+++ src/v2/components/images/component-image.tsx
@@ -4,7 +4,7 @@ import { getImageUrl } from 'src/shared-logic/functions';
 
 export const ComponentImage = ({ alliance, size = 'medium' }: { alliance: Alliance; size?: 'small' | 'medium' }) => {
     const sizePx = size === 'medium' ? 35 : 25;
-    const image = getImageUrl(`components/${alliance.toLowerCase()}.png`);
+    const image = getImageUrl(`mowComponents/${alliance.toLowerCase()}.png`);
 
     return <img loading={'lazy'} style={{ pointerEvents: 'none' }} src={image} height={sizePx} alt={alliance} />;
 };
