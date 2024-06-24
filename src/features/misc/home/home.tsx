7799ffd466850b56ed722bdb1ae1519be349392a
Severyn Vehera <severyn.vehera@shamaym.com>
2024-06-24 16:05:45 +0300

    update LRE date

--- src/features/misc/home/home.tsx
+++ src/features/misc/home/home.tsx
@@ -9,13 +9,13 @@ import { isMobile } from 'react-device-detect';
 import { sum } from 'lodash';
 import { MiscIcon } from '../../../shared-components/misc-icon';
 
-import lre from 'src/assets/legendary-events/Kharn.json';
+import lre from 'src/assets/legendary-events/Ragnar.json';
 import { PersonalGoalType } from 'src/models/enums';
 
 export const Home = () => {
     const navigate = useNavigate();
     const { goals, dailyRaids } = useContext(StoreContext);
-    const nextLeMenuItem = menuItemById['kharn'];
+    const nextLeMenuItem = menuItemById['ragnar'];
     const goalsMenuItem = menuItemById['goals'];
     const dailyRaidsMenuItem = menuItemById['dailyRaids'];
     const eventsCalendarUrl = 'https://tacticucplannerstorage.blob.core.windows.net/files/events-calendar.png';
