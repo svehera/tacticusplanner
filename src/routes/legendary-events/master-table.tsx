589571c4168dc8a2eabed3006dd9e3c76d777a29
Severyn Vehera <severyn.vehera@shamaym.com>
2024-06-26 00:30:27 +0300

    update names

--- src/routes/legendary-events/master-table.tsx
+++ src/routes/legendary-events/master-table.tsx
@@ -100,7 +100,7 @@ export const MasterTable = () => {
                 ],
             },
             ...activeLegendaryEvents.map(eventId => ({
-                headerName: LegendaryEventEnum[eventId],
+                headerName: eventId === LegendaryEventEnum.Mephiston ? 'Blood Angel???' : LegendaryEventEnum[eventId],
                 children: [
                     {
                         field: eventId + 'points',
@@ -362,13 +362,22 @@ export const MasterTable = () => {
                         onChange={handleLESelectChange}
                         multiple
                         input={<OutlinedInput label="Legendary Events" />}
-                        renderValue={selected => selected.map(x => LegendaryEventEnum[x]).join(', ')}>
+                        renderValue={selected =>
+                            selected
+                                .map(x => {
+                                    return x === LegendaryEventEnum.Mephiston
+                                        ? 'Blood Angel???'
+                                        : LegendaryEventEnum[x];
+                                })
+                                .join(', ')
+                        }>
                         <MenuItem value={LegendaryEventEnum.Mephiston}>
                             <Checkbox checked={activeLegendaryEvents.indexOf(LegendaryEventEnum.Mephiston) > -1} />
                             <ListItemIcon>
                                 <CharacterImage icon={'unset.png'} imageSize={30} />
                             </ListItemIcon>
-                            <ListItemText primary={LegendaryEventEnum[LegendaryEventEnum.Mephiston]} />
+                            {/*<ListItemText primary={LegendaryEventEnum[LegendaryEventEnum.Mephiston]} />*/}
+                            <ListItemText primary={'Blood Angel???'} />
                         </MenuItem>
                         <MenuItem value={LegendaryEventEnum.Kharn}>
                             <Checkbox checked={activeLegendaryEvents.indexOf(LegendaryEventEnum.Kharn) > -1} />
