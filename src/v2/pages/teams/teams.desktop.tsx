276f00cace3449bc130b07b7d733a08d70e049ec
Severyn Vehera <severyn.vehera@shamaym.com>
2024-07-26 17:35:12 +0300

    add quick search

--- src/v2/pages/teams/teams.desktop.tsx
+++ src/v2/pages/teams/teams.desktop.tsx
@@ -1,16 +1,19 @@
-﻿import React, { useContext } from 'react';
+﻿import React, { useContext, useMemo } from 'react';
 import AddIcon from '@mui/icons-material/Add';
-import { Fab } from '@mui/material';
+import { Fab, TextField } from '@mui/material';
 import { AddTeamDialog } from 'src/v2/features/teams/components/add-team.dialog';
 import { DispatchContext, StoreContext } from 'src/reducers/store.provider';
 import { CharactersViewContext } from 'src/v2/features/characters/characters-view.context';
 import { IPersonalTeam } from 'src/v2/features/teams/teams.models';
 import { TeamsGrid } from 'src/v2/features/teams/components/teams-grid';
 import { EditTeamDialog } from 'src/v2/features/teams/components/edit-team.dialog';
+import { useDebounceValue } from 'usehooks-ts';
+import { guildRaidSubModes, taSubModes } from 'src/v2/features/teams/teams.constants';
 
 export const Teams = () => {
     const dispatch = useContext(DispatchContext);
     const { mows, characters, viewPreferences, teams } = useContext(StoreContext);
+    const [quickFilter, setQuickFilter] = useDebounceValue('', 300);
     const [openCreateTeamDialog, setOpenCreateTeamDialog] = React.useState(false);
     const [editTeam, setEditTeam] = React.useState<IPersonalTeam | null>(null);
 
@@ -29,20 +32,50 @@ export const Teams = () => {
         }
     };
 
+    const filteredTeams = useMemo(() => {
+        const searchTerm = quickFilter.toLowerCase();
+        const modes = [...taSubModes, ...guildRaidSubModes];
+
+        if (!searchTerm) {
+            return teams;
+        }
+
+        return teams.filter(team => {
+            const nameMatched = () => team.name.toLowerCase().includes(searchTerm);
+            const modeMatched = () => {
+                const modsNames = team.subModes.map(mod =>
+                    (modes.find(x => x.value === mod)?.label ?? '').toLowerCase()
+                );
+                return modsNames.some(mod => mod.includes(searchTerm));
+            };
+
+            return nameMatched() || modeMatched();
+        });
+    }, [teams, quickFilter]);
+
     return (
         <>
-            <Fab
-                variant="extended"
-                size="small"
-                color="primary"
-                aria-label="add"
-                onClick={() => setOpenCreateTeamDialog(true)}>
-                <AddIcon />
-                New team
-            </Fab>
+            <div className="flex-box gap10">
+                <Fab
+                    variant="extended"
+                    size="small"
+                    color="primary"
+                    aria-label="add"
+                    onClick={() => setOpenCreateTeamDialog(true)}>
+                    <AddIcon />
+                    New team
+                </Fab>
+                <TextField
+                    size="small"
+                    sx={{ margin: '10px', width: '220px' }}
+                    label="Quick Search"
+                    variant="outlined"
+                    onChange={event => setQuickFilter(event.target.value)}
+                />
+            </div>
             <CharactersViewContext.Provider value={viewPreferences}>
                 <TeamsGrid
-                    teams={teams}
+                    teams={filteredTeams}
                     characters={characters}
                     mows={mows}
                     editTeam={setEditTeam}
