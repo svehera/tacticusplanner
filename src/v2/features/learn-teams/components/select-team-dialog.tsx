2bdf59b01906173bff0cb4d9bd297e4dc42f4e99
Severyn Vehera <severyn.vehera@shamaym.com>
2024-08-19 00:15:07 +0300

    Update team select logic

--- src/v2/features/learn-teams/components/select-team-dialog.tsx
+++ src/v2/features/learn-teams/components/select-team-dialog.tsx
@@ -3,7 +3,7 @@ import { useDebounceValue } from 'usehooks-ts';
 import { isMobile } from 'react-device-detect';
 import { cloneDeep } from 'lodash';
 
-import { DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
+import { DialogActions, DialogContent, DialogTitle, TextField, Typography } from '@mui/material';
 import Dialog from '@mui/material/Dialog';
 import Button from '@mui/material/Button';
 import { Alliance } from 'src/models/enums';
@@ -15,6 +15,7 @@ import { ITeamSlot } from 'src/v2/features/learn-teams/learn-teams.models';
 import { TeamSlotEdit } from 'src/v2/features/learn-teams/components/team-slot-edit';
 import { UnitsGrid } from 'src/v2/features/learn-teams/components/units-grid';
 import { SlotType } from 'src/v2/features/learn-teams/learn-teams.enums';
+import InfoIcon from '@mui/icons-material/Info';
 
 type Props = {
     units: IUnit[];
@@ -75,6 +76,40 @@ export const SelectTeamDialog: React.FC<Props> = ({ onClose, slots, units }) => 
 
                 editedSlot.unitIds[editSlotData.index] = unit.id;
 
+                if (editedSlot.slotType === SlotType.core) {
+                    const nextSlot = curr.find(x => x.slotNumber === editSlotData.slotNumber + 1);
+                    if (nextSlot && !nextSlot.unitIds.filter(x => !!x).length && nextSlot.slotType !== SlotType.none) {
+                        setEditSlotData({
+                            index: 0,
+                            slotNumber: editSlotData.slotNumber + 1,
+                        });
+                    }
+                }
+
+                if (editedSlot.slotType === SlotType.flex) {
+                    const nextSlot = editedSlot.unitIds[editSlotData.index + 1];
+                    if (!nextSlot && editSlotData.index <= 1) {
+                        setEditSlotData({
+                            index: editSlotData.index + 1,
+                            slotNumber: editSlotData.slotNumber,
+                        });
+                    }
+
+                    if (editSlotData.index === 2) {
+                        const nextSlot = curr.find(x => x.slotNumber === editSlotData.slotNumber + 1);
+                        if (
+                            nextSlot &&
+                            !nextSlot.unitIds.filter(x => !!x).length &&
+                            nextSlot.slotType !== SlotType.none
+                        ) {
+                            setEditSlotData({
+                                index: 0,
+                                slotNumber: editSlotData.slotNumber + 1,
+                            });
+                        }
+                    }
+                }
+
                 return [...curr];
             });
         }
@@ -132,10 +167,41 @@ export const SelectTeamDialog: React.FC<Props> = ({ onClose, slots, units }) => 
         setFilterByVar(value[0] as FilterBy);
     };
 
+    const handleTeamUnitClick = (slotNumber: number, index: number) => {
+        const isTheSameSlot = editSlotData.slotNumber === slotNumber && editSlotData.index === index;
+        if (isTheSameSlot) {
+            const slot = lineup.find(x => x.slotNumber === slotNumber);
+            const isOccupiedSlot = !!slot && !!slot.unitIds[index]?.length;
+            if (isOccupiedSlot) {
+                setLineup(curr => {
+                    const editedSlot = curr.find(x => x.slotNumber === slotNumber);
+                    if (!editedSlot) {
+                        return curr;
+                    }
+
+                    delete editedSlot.unitIds[index];
+
+                    return [...curr];
+                });
+            }
+        } else {
+            setEditSlotData({
+                slotNumber,
+                index,
+            });
+        }
+    };
+
     return (
         <Dialog open={true} onClose={cancel} fullWidth fullScreen={isMobile}>
             <DialogTitle>
                 Select team
+                <Typography className="flex-box" style={{ margin: '10px 0' }}>
+                    <InfoIcon color="primary" />
+                    <span>
+                        Toggle between &ldquo;Core&ldquo; and &ldquo;Flex&ldquo; slot types by clicking on the slot type
+                    </span>
+                </Typography>
                 <div className="flex-box gap5 start">
                     {lineup.map(slot => (
                         <TeamSlotEdit
@@ -144,12 +210,7 @@ export const SelectTeamDialog: React.FC<Props> = ({ onClose, slots, units }) => 
                             slot={slot}
                             editable={true}
                             selectedIndex={editSlotData.slotNumber === slot.slotNumber ? editSlotData.index : -1}
-                            editSlot={index =>
-                                setEditSlotData({
-                                    slotNumber: slot.slotNumber,
-                                    index,
-                                })
-                            }
+                            editSlot={index => handleTeamUnitClick(slot.slotNumber, index)}
                             editType={type => handleSlotTypeChange(slot.slotNumber, type)}
                         />
                     ))}
