382bb8fcf2b24bfa192d8c2fa3f9ae1d125f42d2
Severyn Vehera <severyn.vehera@shamaym.com>
2024-08-22 00:10:07 +0300

    hide new character

--- src/services/static-data.service.ts
+++ src/services/static-data.service.ts
@@ -73,7 +73,9 @@ export class StaticDataService {
 
     static readonly campaignsComposed: Record<string, ICampaignBattleComposed> = CampaignsService.campaignsComposed;
 
-    static readonly unitsData: IUnitData[] = (unitsData as UnitDataRaw[]).map(this.convertUnitData);
+    static readonly unitsData: IUnitData[] = (unitsData as UnitDataRaw[])
+        .map(this.convertUnitData)
+        .filter(x => !['Lucien', 'Titus'].includes(x.name));
     static readonly campaignsGrouped: Record<string, ICampaignBattleComposed[]> = this.getCampaignGrouped();
     static readonly recipeDataFull: IRecipeDataFull = this.convertRecipeData();
 
