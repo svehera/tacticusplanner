22e37a192d536da5ae3632073a1d1d604c4e9f46
Severyn <severyn.vehera@gmail.com>
2024-11-14 18:21:55 +0200

    add whats new

--- src/v2/features/dirty-dozen/dirty-dozen.models.ts
+++ src/v2/features/dirty-dozen/dirty-dozen.models.ts
@@ -1,13 +1,13 @@
 ﻿export interface IDirtyDozenChar {
     Name: string;
     Position: number;
-    GRTyranid: number;
-    GRNecron: number;
-    GROrk: number;
-    GRMortarion: number;
-    GRScreamer: number;
-    GRRogalDorn: number;
-    GRAvatar: number;
+    // GRTyranid: number;
+    // GRNecron: number;
+    // GROrk: number;
+    // GRMortarion: number;
+    // GRScreamer: number;
+    // GRRogalDorn: number;
+    // GRAvatar: number;
     GuildRaid: number;
     GuildWar: number;
     Horde: number;
