7eb40e4acf242e11386c773b5b44fbfbfeb1579e
unrstuart <cpunerd@gmail.com>
2025-01-05 07:02:48 +0200

    Show the tiles of the required characters in the campaign-progress page.

--- src/routes/campaigns-progress.tsx
+++ src/routes/campaigns-progress.tsx
@@ -1,9 +1,16 @@
-﻿import React, { useContext } from 'react';
+﻿import { groupBy } from 'lodash';
+import React, { useContext } from 'react';
 import { Box, Grid, Input, Slider } from '@mui/material';
+
 import Typography from '@mui/material/Typography';
 import { DispatchContext, StoreContext } from '../reducers/store.provider';
 import { campaignsNames } from '../models/constants';
 import { CampaignImage } from 'src/v2/components/images/campaign-image';
+import { CharacterTile } from 'src/v2/features/characters/components/character-tile';
+import { ICharacter2 } from 'src/models/interfaces';
+import { UnitType } from 'src/v2/features/characters/units.enums';
+import { CharactersService } from 'src/v2/features/characters/characters.service';
+import { CharactersFilterBy } from 'src/v2/features/characters/enums/characters-filter-by';
 
 export const CampaignsProgress = () => {
     const { campaignsProgress } = useContext(StoreContext);
@@ -52,6 +59,9 @@ export const CampaignProgress = ({
     value: number;
     setValue: (value: number) => void;
 }) => {
+    const { characters } = useContext(StoreContext);
+    const dispatch = useContext(DispatchContext);
+
     const handleSliderChange = (event: Event, newValue: number | number[]) => {
         setValue(newValue as number);
     };
@@ -68,6 +78,51 @@ export const CampaignProgress = ({
         }
     };
 
+    const requiredFaction: Map<string, string> = new Map([
+        ['Indomitus', 'Ultramarines'],
+        ['Indomitus Mirror', 'Necrons'],
+        ['Fall of Cadia', 'Black Legion'],
+        ['Fall of Cadia Mirror', 'Astra Militarum'],
+        ['Octarius', 'Orks'],
+        ['Octarius Mirror', 'Black Templars'],
+        ['Saim-Hann', 'Aeldari'],
+        ['Saim-Hann Mirror', 'Thousand Sons'],
+    ]);
+    const alliances: Map<string, string> = new Map([
+        ['Indomitus', 'Imperial'],
+        ['Indomitus Mirror', 'Necrons'],
+        ['Fall of Cadia', 'Chaos'],
+        ['Fall of Cadia Mirror', 'Imperial'],
+        ['Octarius', 'Orks'],
+        ['Octarius Mirror', 'Imperial'],
+        ['Saim-Hann', 'Aeldari'],
+        ['Saim-Hann Mirror', 'Chaos'],
+    ]);
+
+    function getBaseCampaign(campaign: string): string {
+        let base: string = campaign;
+        if (base.endsWith('Elite')) {
+            base = base.substring(0, base.length - 6);
+        }
+        return base;
+    }
+
+    function getCampaignFaction(campaign: string): string {
+        const faction = requiredFaction.get(getBaseCampaign(campaign));
+        return faction != null ? faction : 'Ultramarines';
+    }
+
+    function getRequiredCharacters(campaign: string): ICharacter2[] {
+        let chars: ICharacter2[] = [];
+        const faction: string = getCampaignFaction(getBaseCampaign(campaign));
+        groupBy(CharactersService.filterUnits(characters, CharactersFilterBy.None, ''), 'faction')[faction].forEach(
+            unit => {
+                const character = unit as ICharacter2;
+                if (character.requiredInCampaign) chars = chars.concat(character);
+            }
+        );
+        return chars;
+    }
     return (
         <Box sx={{ width: 250, opacity: value === max ? 0.5 : 1 }}>
             <Typography id="input-slider" gutterBottom>
@@ -100,6 +155,17 @@ export const CampaignProgress = ({
                         }}
                     />
                 </Grid>
+            </Grid>
+            <Grid>
+                {getRequiredCharacters(campaign).map(unit => {
+                    if (unit.unitType === UnitType.character) {
+                        return (
+                            <div key={"'char_tile_" + unit.name + "'"} style={{ float: 'left' }}>
+                                <CharacterTile key={unit.name} character={unit} onCharacterClick={() => {}} />
+                            </div>
+                        );
+                    }
+                })}
             </Grid>
         </Box>
     );
