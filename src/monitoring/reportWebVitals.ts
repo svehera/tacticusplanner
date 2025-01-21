117a5d84b21a36eddea186f3a0ddf136bfa51eab
Severyn <severyn.vehera@gmail.com>
2025-01-21 17:22:34 +0200

    update data collection

--- src/monitoring/reportWebVitals.ts
+++ src/monitoring/reportWebVitals.ts
@@ -1,19 +1,45 @@
-import { onCLS, onINP, onLCP, onTTFB, onFCP } from 'web-vitals';
+import { onCLS, onINP, onLCP, onTTFB, onFCP } from 'web-vitals/attribution';
 import analytics from './analytics';
-import { Metric } from 'web-vitals/src/types/base';
+import { MetricWithAttribution } from 'web-vitals/src/types/base';
 
-const reportWebVitals = (onReport: (metric: Metric) => void) => {
-    const sendMetric = (metric: Metric) => {
+const reportWebVitals = (onReport: (metric: MetricWithAttribution) => void) => {
+    const sendMetric = (metric: MetricWithAttribution) => {
         if (onReport) {
             onReport(metric); // Optional: handle metrics in your own function
         }
+        const eventParams = {
+            // Built-in params:
+            value: metric.delta, // Use `delta` so the value can be summed.
+            // Custom params:
+            metric_id: metric.id, // Needed to aggregate events.
+            metric_value: metric.value, // Optional.
+            metric_delta: metric.delta, // Optional.
+            debug_target: '',
+        };
+
+        switch (metric.name) {
+            case 'CLS':
+                eventParams.debug_target = metric.attribution.largestShiftTarget ?? '';
+                break;
+            case 'INP':
+                eventParams.debug_target = metric.attribution.interactionTarget;
+                break;
+            case 'LCP':
+                eventParams.debug_target = metric.attribution.element ?? '';
+                break;
+            case 'TTFB':
+                eventParams.debug_target = metric.attribution.navigationEntry?.name ?? '';
+                break;
+            case 'FCP':
+                eventParams.debug_target = metric.attribution.fcpEntry?.name ?? '';
+                break;
+        }
+
         // Track the metric to Google Analytics via `analytics`
-        analytics.track('Web Vitals', {
+        analytics.track(metric.name, {
             category: 'Web Vitals',
             label: metric.name, // Metric name (e.g., CLS, LCP)
-            value: metric.value, // Metric value
-            id: metric.id, // Metric ID
-            delta: metric.delta,
+            ...eventParams,
         });
     };
 
