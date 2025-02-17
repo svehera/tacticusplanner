8184e86fcca60056c2efcb037a01884271b08744
Severyn <severyn.vehera@gmail.com>
2025-02-17 19:46:20 +0200

    Remove unnecessary files

--- globals.d.ts
+++
@@ -1,8 +0,0 @@
-interface BeforeInstallPromptEvent extends Event {
-    readonly platform: string; // The platform (e.g., 'android', 'windows')
-    readonly userChoice: Promise<{
-        outcome: 'accepted' | 'dismissed'; // Whether the user accepted or dismissed the installation prompt
-        platform: string; // The platform on which the prompt was triggered
-    }>;
-    prompt: () => Promise<void>; // Function to trigger the install prompt
-}
