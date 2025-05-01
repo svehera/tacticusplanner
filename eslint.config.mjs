d7c6b2cef770d586cc954dd43634d3383537825e
Severyn <severyn.vehera@gmail.com>
2025-05-01 23:36:17 +0300

    add boundaries rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
+import boundaries from 'eslint-plugin-boundaries';
 import * as pluginImportX from 'eslint-plugin-import-x';
 import prettier from 'eslint-plugin-prettier';
 import react from 'eslint-plugin-react';
@@ -46,7 +47,8 @@ export default [
             react,
             'react-refresh': reactRefresh,
             'react-compiler': reactCompiler,
-            prettier,     
+            boundaries,
+            prettier,
         },
 
         languageOptions: {
@@ -63,6 +65,20 @@ export default [
             react: {
                 version: 'detect',
             },
+            'import/resolver': {
+                typescript: {
+                    alwaysTryTypes: true,
+                    project: './tsconfig.json',
+                },
+            },
+            'boundaries/elements': [
+                { type: 'app', pattern: '0-app/*' },
+                { type: 'pages', pattern: '1-pages/*' },
+                { type: 'widgets', pattern: '2-widgets/*' },
+                { type: 'features', pattern: '3-features/*' },
+                { type: 'entities', pattern: '4-entities/*' },
+                { type: 'shared', pattern: '5-shared/*' },
+            ]
         },
 
         rules: {
@@ -81,26 +97,57 @@ export default [
             'react-refresh/only-export-components': 'error',
             'react-compiler/react-compiler': 'error',
             'import-x/order': [
-            2,
-            {
-                alphabetize: {
-                    order: 'asc',
-                    caseInsensitive: true,
-                },
-                pathGroupsExcludedImportTypes: ['builtin'],
-                groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+                'error',
+                {
+                    alphabetize: {
+                        order: 'asc',
+                        caseInsensitive: true,
+                    },
+                    pathGroupsExcludedImportTypes: ['builtin'],
+                    groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
 
-                // experimental features
-                'newlines-between': 'always',
-                pathGroups: REVERSED_FS_LAYERS.map(
-                    (layer) => ({
-                        pattern: `**/?(*)${layer}{,/**}`,
-                        group: 'internal',
-                        position: 'after',
-                    }),
-                ),
-            },
-        ],
+                    // experimental features
+                    'newlines-between': 'always',
+                    pathGroups: REVERSED_FS_LAYERS.map(
+                        (layer) => ({
+                            pattern: `**/?(*)${layer}{,/**}`,
+                            group: 'internal',
+                            position: 'after',
+                        }),
+                    ),
+                },
+
+            ],
+            'boundaries/element-types': ['error', {
+                default: 'disallow',
+                message: '${file.type} is not allowed to import ${dependency.type}',
+                rules: [
+                    {
+                        from: 'app',
+                        allow: ['app', 'shared', 'entities', 'features', 'widgets', 'pages'],
+                    },
+                    {
+                        from: 'pages',
+                        allow: ['shared', 'entities', 'features', 'widgets'],
+                    },
+                    {
+                        from: 'widgets',
+                        allow: ['shared', 'entities', 'features'],
+                    },
+                    {
+                        from: 'features',
+                        allow: ['shared', 'entities'],
+                    },
+                    {
+                        from: 'entities',
+                        allow: ['shared'],
+                    },
+                    {
+                        from: 'shared',
+                        allow: [],
+                    }
+                ]
+            }]
         },
     },
 ];
