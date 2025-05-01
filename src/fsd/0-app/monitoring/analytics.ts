ef13ace3086cb38597bfdb218068478670ba3472
Severyn <severyn.vehera@gmail.com>
2025-05-01 22:26:49 +0300

    fix build

--- src/fsd/0-app/monitoring/analytics.ts
+++ src/fsd/0-app/monitoring/analytics.ts
@@ -1,6 +1,5 @@
 import googleAnalytics from '@analytics/google-analytics';
 import Analytics from 'analytics';
-// @ts-expect-error googleAnalytics does not provide types
 
 const analytics = Analytics({
     app: 'Tacticus Planner',
