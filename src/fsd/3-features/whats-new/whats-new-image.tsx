73439db0c34782ad207988a618019789027729b4
Severyn <33064347+svehera@users.noreply.github.com>
2025-05-02 23:22:36 +0300

    Update src/fsd/3-features/whats-new/whats-new-image.tsx
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/fsd/3-features/whats-new/whats-new-image.tsx
+++ src/fsd/3-features/whats-new/whats-new-image.tsx
@@ -1,6 +1,7 @@
 ﻿import React from 'react';
 
-import { getImageUrl } from 'src/fsd/5-shared/ui';
+-import { getImageUrl } from 'src/fsd/5-shared/ui';
++import { getImageUrl } from '@/fsd/5-shared/ui';
 
 export const WhatsNewImage = ({ path, imageSize }: { path: string; imageSize?: number }) => {
     const image = getImageUrl(`whatsnew/${path}`);
