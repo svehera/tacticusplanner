338ec8da3a746c3146ca79d6e05f1f5ad4ddde07
show <ian.the.laser403@gmail.com>
2025-06-28 13:35:42 +1000

    Scaffold learn/equipment route

---
+++ src/fsd/1-pages/learn-equipment/equipment.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Equipment = () => {
+    return (
+        <div>
+            <h1>EquipmentTable</h1>
+        </div>
+    );
+};
---
+++ src/fsd/1-pages/learn-equipment/index.ts
@@ -0,0 +1,1 @@
+export { Equipment } from './equipment';
