77bd12bdc3a5c43d05918778fefc6d13695c5fc0
show <ian.the.laser403@gmail.com>
2025-07-15 15:22:26 +1000

    Include Tau campaign in filter function

--- src/fsd/4-entities/campaign/campaigns.constants.ts
+++ src/fsd/4-entities/campaign/campaigns.constants.ts
@@ -276,7 +276,11 @@ export const campaignsByGroup: Record<string, Campaign[]> = mapValues(groupBy(ca
     value.map(x => x.id)
 );
 
-const campaignEvents: CampaignGroupType[] = [CampaignGroupType.adMechCE, CampaignGroupType.tyranidCE];
+const campaignEvents: CampaignGroupType[] = [
+    CampaignGroupType.adMechCE,
+    CampaignGroupType.tyranidCE,
+    CampaignGroupType.tauCE,
+];
 
 export const campaignEventsLocations: Campaign[] = campaignsList
     .filter(x => campaignEvents.includes(x.groupType))
