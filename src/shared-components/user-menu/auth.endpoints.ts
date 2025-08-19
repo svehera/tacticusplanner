42d7dbbc4457d7439b1fa099a134f9eec1ea384f
show <ian.the.laser403@gmail.com>
2025-08-20 09:08:35 +1000

    Improved UX for signup errors
    The app now tells the user if they attempt
    to register with a usernames that's already taken

--- src/shared-components/user-menu/auth.endpoints.ts
+++ src/shared-components/user-menu/auth.endpoints.ts
@@ -2,8 +2,14 @@ import { IErrorResponse, makeApiCall } from '@/fsd/5-shared/api';
 
 import { ILoginResponse, IRegistrationResponse } from './auth.model';
 
-export const registerUser = (username: string, password: string) =>
-    makeApiCall<IRegistrationResponse, IErrorResponse>('POST', 'RegisterUser', { username, password } as any);
+export const registerUser = async (username: string, password: string) => {
+    const { data, error } = await makeApiCall<IRegistrationResponse, IErrorResponse>('POST', 'RegisterUser', {
+        username,
+        password,
+    } as any);
+    if (error) throw error;
+    return { data, error: null };
+};
 
 export const loginUser = (username: string, password: string) =>
     makeApiCall<ILoginResponse, IErrorResponse>('POST', 'LoginUser', { username, password } as any);
