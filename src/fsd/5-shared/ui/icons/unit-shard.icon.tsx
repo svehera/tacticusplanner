af003a21aa0e2309a8d61b21719285e9f3baa122
show <ian.the.laser403@gmail.com>
2025-08-19 12:31:22 +1000

    Refactor UnitShardIcon to use Snowprint assets
    Previously, a unit's round image had the wreath border included.
    
    Now we're using Snowprint's raw assets, we needed to use the wreath border
    as an underlay for the unit's round image, which is a bit more fiddly.

--- src/fsd/5-shared/ui/icons/unit-shard.icon.tsx
+++ src/fsd/5-shared/ui/icons/unit-shard.icon.tsx
@@ -18,14 +18,53 @@ export const UnitShardIcon = ({
 }) => {
     if (!icon) return;
     const imageUrl = getImageUrl(`${icon.replace('.webp', '.png')}`);
+    const borderImageUrl = getImageUrl('snowprint_assets/frames/ui_icon_character_shard_empty.png');
+    const defaultWidth = 50;
+    width = width ?? defaultWidth;
+    height = height ?? defaultWidth;
+    const roundImgOriginal = { width: 148, height: 148 };
+    // The wreath border image was originally 242x210px, with an aspect ratio of 1.15238095.
+    // I.e. not a circle/square with equal width and height.
+    // To allow the unit's roundIcon to overlay, while allowing dynamic sizing, we're storing the
+    // aspect ratio here as a constant. If the underlying wreath image changes, this will need to change.
+    const borderImgOriginal = { width: 242, height: 210 };
+    const borderImgScale = Math.min(width / borderImgOriginal.width, height / borderImgOriginal.height);
+    // `overlayScale` is a magic number that was manually adjusted to center the roundIcon over
+    // the wreath border with minimal gaps.
+    const overlayScale = 1.25;
+    const overlaySize = roundImgOriginal.width * borderImgScale * overlayScale;
 
     const image = (
-        <img
-            loading={'lazy'}
-            style={{ pointerEvents: 'none', borderRadius: '50%', maxWidth: width ?? 50, maxHeight: height ?? 50 }}
-            src={imageUrl}
-            alt={name ?? icon}
-        />
+        <div style={{ position: 'relative', display: 'inline-block', width, height }}>
+            <img
+                loading={'lazy'}
+                style={{ display: 'block', width, height, borderRadius: '50%', objectFit: 'contain' }}
+                src={borderImageUrl}
+                alt="character border"
+            />
+            <div
+                style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    pointerEvents: 'none',
+                }}>
+                <img
+                    loading={'lazy'}
+                    style={{
+                        width: overlaySize,
+                        height: overlaySize,
+                    }}
+                    src={imageUrl}
+                    alt={name ?? icon}
+                />
+            </div>
+        </div>
     );
 
     return tooltip ? <AccessibleTooltip title={tooltip}>{image}</AccessibleTooltip> : image;
