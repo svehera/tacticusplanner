a249b0e0e46ece5bab00e5fc413ec4adc82f0460
show <ian.the.laser403@gmail.com>
2025-08-20 10:13:48 +1000

    Fix type info for auth API calls
    Removes type casting that was turning off
    type checks, and sets data body types to match
    what we're using

--- src/shared-components/user-menu/auth.endpoints.ts
+++ src/shared-components/user-menu/auth.endpoints.ts
@@ -1,15 +1,22 @@
-import { IErrorResponse, makeApiCall } from '@/fsd/5-shared/api';
+import { makeApiCall } from '@/fsd/5-shared/api';
 
 import { ILoginResponse, IRegistrationResponse } from './auth.model';
 
 export const registerUser = async (username: string, password: string) => {
-    const { data, error } = await makeApiCall<IRegistrationResponse, IErrorResponse>('POST', 'RegisterUser', {
-        username,
-        password,
-    } as any);
+    const { data, error } = await makeApiCall<IRegistrationResponse, { username: string; password: string }>(
+        'POST',
+        'RegisterUser',
+        {
+            username,
+            password,
+        }
+    );
     if (error) throw error;
     return { data, error: null };
 };
 
 export const loginUser = (username: string, password: string) =>
-    makeApiCall<ILoginResponse, IErrorResponse>('POST', 'LoginUser', { username, password } as any);
+    makeApiCall<ILoginResponse, { username: string; password: string }>('POST', 'LoginUser', {
+        username,
+        password,
+    });
