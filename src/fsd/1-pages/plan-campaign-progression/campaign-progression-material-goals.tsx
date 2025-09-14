abec88393838e7aec12f593eee796827f94206b5
unrstuart <cpunerd@gmail.com>
2025-09-14 21:25:14 +0200

    Fix a few more faction name != id errors

--- src/fsd/1-pages/plan-campaign-progression/campaign-progression-material-goals.tsx
+++ src/fsd/1-pages/plan-campaign-progression/campaign-progression-material-goals.tsx
@@ -80,7 +80,7 @@ export const CampaignProgressionMaterialGoals: React.FC<Props> = ({ campaignData
         const node = getCheapestNode(material);
         if (!node) return '(unfarmable)';
         return (
-            material +
+            UpgradesService.getUpgrade(material).label +
             ' is currently farmable from ' +
             node.id +
             '. You need ' +
