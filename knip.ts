6a33b7abd5f9fee234a1a2159d935b7997729e34
Joel Tannas <jtannas@gmail.com>
2025-10-13 18:03:13 -0600

    apply correction from later GH issue comment

--- knip.ts
+++ knip.ts
@@ -3,7 +3,7 @@ import type { KnipConfig } from 'knip';
 const config: KnipConfig = {
     compilers: {
         // https://github.com/webpro-nl/knip/issues/1008#issuecomment-3207756199
-        css: (text: string) => [...text.matchAll(/(?<=@)(import|plugin)[^;]+/g)].join('\n').replace('plugin', 'import'),
+        css: (text: string) => [...text.replaceAll('plugin', 'import').matchAll(/(?<=@)import[^;]+/g)].join('\n'),
     },
     entry: ['src/fsd/0-app/index.tsx'],
     project: ['**/*.{js,ts,jsx,tsx,css}'],
