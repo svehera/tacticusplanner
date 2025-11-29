5321028d74268f87d6c5fc92449efe8249086cae
rosogac <rosogac@gmail.com>
2025-11-29 19:22:25 +0200

    Add target level tooltip to table view goal page

--- src/v2/features/goals/xp-total.tsx
+++ src/v2/features/goals/xp-total.tsx
@@ -1,12 +1,11 @@
-﻿import { Info } from '@mui/icons-material';
-import React from 'react';
+﻿import React from 'react';
 
 import { numberToThousandsString } from '@/fsd/5-shared/lib/number-to-thousands-string';
-import { AccessibleTooltip } from '@/fsd/5-shared/ui';
 
 import { IXpEstimate } from 'src/v2/features/characters/characters.models';
+import { XpTooltip } from 'src/v2/features/goals/xp-tooltip';
 
-export const XpTotal: React.FC<IXpEstimate> = ({ legendaryBooks, currentLevel, targetLevel, xpLeft, gold }) => {
+export const XpTotal: React.FC<IXpEstimate> = ({ legendaryBooks, gold, currentLevel, targetLevel, xpLeft }) => {
     return (
         <div className="flex-box gap5">
             <span>
@@ -15,18 +14,13 @@ export const XpTotal: React.FC<IXpEstimate> = ({ legendaryBooks, currentLevel, t
             <span>
                 <b>{numberToThousandsString(gold)}</b> Gold
             </span>
-            <AccessibleTooltip
-                title={
-                    <span>
-                        Current level: {currentLevel}
-                        <br />
-                        Target level: {targetLevel}
-                        <br />
-                        XP left: {numberToThousandsString(xpLeft)}
-                    </span>
-                }>
-                <Info color="primary" />
-            </AccessibleTooltip>
+            <XpTooltip
+                legendaryBooks={legendaryBooks}
+                gold={gold}
+                currentLevel={currentLevel}
+                targetLevel={targetLevel}
+                xpLeft={xpLeft}
+            />
         </div>
     );
 };
