f40d2319ab07cfda8ce6bb81a2be82c70a7b93ef
Redwyne92 <gbs9221@gmail.com>
2025-12-06 22:19:22 +0100

    Fix Need To Level Filter

--- src/v2/features/characters/functions/need-to-level.ts
+++ src/v2/features/characters/functions/need-to-level.ts
@@ -1,6 +1,6 @@
 ﻿import { rankToLevel } from 'src/models/constants';
 
-import { UnitType, Rank } from '@/fsd/5-shared/model';
+import { UnitType, Rank, Rarity } from '@/fsd/5-shared/model';
 
 import { IUnit } from '@/fsd/4-entities/unit';
 
@@ -13,10 +13,33 @@ export const needToLevelCharacter = (unit: IUnit) => {
 
     const isUnlocked = unit.rank > Rank.Locked;
     const needToAscend = needToAscendCharacter(unit);
+
+    var maxRank;
+    switch (unit.rarity) {
+        case Rarity.Common:
+            maxRank  = Rank.Iron1;
+            break;  
+        case Rarity.Uncommon:
+            maxRank  = Rank.Bronze1;
+            break;  
+        case Rarity.Rare:
+            maxRank  = Rank.Silver1;
+            break;
+        case Rarity.Epic:
+            maxRank  = Rank.Gold1;
+            break;  
+        case Rarity.Legendary:
+            maxRank  = Rank.Diamond3;
+            break; 
+        case Rarity.Mythic:
+            maxRank  = Rank.Adamantine3;
+            break; 
+    }
+
+
     return (
         isUnlocked &&
         !needToAscend &&
-        unit.level < rankToLevel[unit.rank] &&
-        6 - (rankToLevel[unit.rank] - unit.level) <= unit.upgrades.length
+        unit.level < rankToLevel[maxRank] 
     );
 };
