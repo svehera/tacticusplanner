c072bfb46d7b82d0298f486318342a809b8bab65
Redwyne92 <gbs9221@gmail.com>
2025-12-07 14:36:42 +0100

    Refactor wing-ready
    Refactor using utilities made for canAscend

--- src/v2/features/characters/functions/wing-ready.ts
+++ src/v2/features/characters/functions/wing-ready.ts
@@ -1,37 +1,20 @@
 ﻿import { Rarity, RarityStars } from '@/fsd/5-shared/model';
 
 import { IUnit } from '@/fsd/4-entities/unit';
+import { getTotalProgressionUntil } from '@/models/constants';
 
 export const wingReady = (unit: IUnit) => {
 
     const isAlreadyWinged = unit.rarity === Rarity.Legendary && unit.stars === RarityStars.OneBlueStar;
     const isAlreadyMythic = unit.rarity === Rarity.Mythic;
 
-    const uncommonBaseShards = 40;
-    const rareBaseShards = uncommonBaseShards + 50;
-    const epicBaseShards = rareBaseShards + 120;
-    const legendaryBaseShards = epicBaseShards + 300;
-    const wingedBaseShards = legendaryBaseShards + 900;
+    if(isAlreadyMythic || isAlreadyWinged) return false;
 
-    var investedShards = 0;
-    switch (unit.rarity) {
-        case Rarity.Uncommon:
-            investedShards  += uncommonBaseShards;
-            break;  
-        case Rarity.Rare:
-            investedShards  += rareBaseShards;
-            break;
-        case Rarity.Epic:
-            investedShards  += epicBaseShards;
-            break;  
-        case Rarity.Legendary:
-            investedShards  += legendaryBaseShards;
-            break; 
-    }
-
+    const totalShardsCurrent = getTotalProgressionUntil(unit.rarity, unit.stars);
+    const totalShardsForNextRarity = getTotalProgressionUntil(Rarity.Legendary, RarityStars.OneBlueStar);
+    const neededShards = (totalShardsForNextRarity.shards ?? 0) - (totalShardsCurrent.shards ?? 0);
+    
     return (
-        !isAlreadyWinged &&
-        !isAlreadyMythic &&
-        ((unit.shards + investedShards) >= wingedBaseShards)
+        (neededShards - unit.shards <= 0)
     );
 };
