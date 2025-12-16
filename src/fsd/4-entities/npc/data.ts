cae5f33eeeb9bee7736d74775e16806217254cba
jtannas <jtannas@gmail.com>
2025-12-16 16:13:54 -0700

    move npc data into shared `data` folder

---
+++ src/fsd/4-entities/npc/data.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import-x/no-internal-modules
+import npcDataJson from '@/data/newNpcData-2.json';
+
+import { INpcDataRaw } from './model';
+
+
+export const npcData: INpcDataRaw[] = npcDataJson;
--- src/fsd/4-entities/npc/data/index.ts
+++
@@ -1,5 +0,0 @@
-import { INpcDataRaw } from '../model';
-
-import npcDataJson from './newNpcData.json';
-
-export const npcData: INpcDataRaw[] = npcDataJson;
