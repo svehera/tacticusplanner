bcdc052a436ada493cc5d8966b37f66e0e45ddc5
jtannas <jtannas@gmail.com>
2025-12-16 16:10:19 -0700

    move equipment data into shared `data` directory

--- src/fsd/4-entities/equipment/data/index.ts
+++
@@ -1,5 +0,0 @@
-import { IEquipmentStatic } from '../model';
-
-import newEquipmentDataJson from './newEquipmentData.json';
-
-export const newEquipmentData: Record<string, IEquipmentStatic> = newEquipmentDataJson;
