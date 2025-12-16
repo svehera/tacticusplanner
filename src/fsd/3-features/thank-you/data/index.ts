1c4f9af6a1d3e3aa8cafaf9994fac363e36f979d
jtannas <jtannas@gmail.com>
2025-12-16 16:08:52 -0700

    move thank-you data into shared `data` folder

--- src/fsd/3-features/thank-you/data/index.ts
+++
@@ -1,7 +0,0 @@
-import { IContributor, IContentCreator } from '../thank-you.model';
-
-import contentCreatorsJson from './contentCreators.json';
-import contributorsJson from './thankYou.json';
-
-export const contributors: IContributor[] = contributorsJson;
-export const contentCreators: IContentCreator[] = contentCreatorsJson;
