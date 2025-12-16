42ee4fc46fed7d9e6dea0ea80e325bada242822a
jtannas <jtannas@gmail.com>
2025-12-16 15:59:01 -0700

    move recipe data into shared `data` directory

---
+++ src/fsd/4-entities/upgrade/data.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import-x/no-internal-modules
+import recipeDataJson from '@/data/newRecipeData.json';
+
+import { IRecipeData } from './model';
+
+export const recipeDataByName: IRecipeData = recipeDataJson;
