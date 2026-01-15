29bba55b995cc98fe9f46ea11b3b978c03e65d4c
jtannas <jtannas@gmail.com>
2026-01-15 13:48:34 -0700

    switch token image loading to imports

--- src/shared-components/images/token-image.tsx
+++ src/shared-components/images/token-image.tsx
@@ -1,36 +1,43 @@
-﻿import { getImageUrl } from 'src/shared-logic/functions';
+﻿import guildRaids from '@/assets/images/modes/_gr.png';
+import guildWar from '@/assets/images/modes/_gw.png';
+import incursion from '@/assets/images/modes/_inc.png';
+import legendaryRelease from '@/assets/images/modes/_lre.png';
+import survival from '@/assets/images/modes/_sur.png';
+import tournamentArena from '@/assets/images/modes/_ta.png';
 
 import { AccessibleTooltip } from '@/fsd/5-shared/ui';
 
 import { GameMode } from '@/fsd/3-features/teams/teams.enums';
 
+const imageMap = {
+    [GameMode.guildRaids]: guildRaids,
+    [GameMode.guildWar]: guildWar,
+    [GameMode.incursion]: incursion,
+    [GameMode.legendaryRelease]: legendaryRelease,
+    [GameMode.survival]: survival,
+    [GameMode.tournamentArena]: tournamentArena,
+} as const;
+
+const textMap = {
+    [GameMode.guildRaids]: 'Guild Raids',
+    [GameMode.guildWar]: 'Guild War',
+    [GameMode.incursion]: 'Incursion',
+    [GameMode.legendaryRelease]: 'Legendary Event',
+    [GameMode.survival]: 'Survival',
+    [GameMode.tournamentArena]: 'Tournament Arena',
+} as const;
+
 export const TokenImage = ({ gameMode }: { gameMode: GameMode }) => {
-    const image = getImageUrl(`modes/${gameMode}.png`);
-
-    let tooltipText = '';
-
-    switch (gameMode) {
-        case GameMode.tournamentArena:
-            tooltipText = 'Tournament Arena';
-            break;
-        case GameMode.guildRaids:
-            tooltipText = 'Guild Raids';
-            break;
-        case GameMode.guildWar:
-            tooltipText = 'Guild War';
-            break;
-        case GameMode.legendaryRelease:
-            tooltipText = 'Legendary Event';
-            break;
-        case GameMode.incursion:
-            tooltipText = 'Incursion';
-            break;
-    }
-
     return (
-        <AccessibleTooltip title={tooltipText}>
+        <AccessibleTooltip title={textMap[gameMode]}>
             <span>
-                <img loading={'lazy'} className="pointer-events-none" src={image} height={40} alt={gameMode} />
+                <img
+                    loading={'lazy'}
+                    className="pointer-events-none"
+                    src={imageMap[gameMode]}
+                    height={40}
+                    alt={textMap[gameMode]}
+                />
             </span>
         </AccessibleTooltip>
     );
