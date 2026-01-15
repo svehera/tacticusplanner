dca5edfb0033c71a9404527b58665bc6b19595d0
jtannas <jtannas@gmail.com>
2026-01-15 13:41:46 -0700

    use import loading for `component-image.tsx`

--- src/fsd/5-shared/ui/icons/component-image.tsx
+++ src/fsd/5-shared/ui/icons/component-image.tsx
@@ -1,10 +1,24 @@
-﻿import { Alliance } from '@/fsd/5-shared/model';
+﻿// eslint-disable-next-line import-x/no-internal-modules
+import chaos from '@/assets/images/mowComponents/resized/chaos.png';
+// eslint-disable-next-line import-x/no-internal-modules
+import imperial from '@/assets/images/mowComponents/resized/imperial.png';
+// eslint-disable-next-line import-x/no-internal-modules
+import xenos from '@/assets/images/mowComponents/resized/xenos.png';
 
-import { getImageUrl } from '../get-image-url';
+import { Alliance } from '@/fsd/5-shared/model';
 
-export const ComponentImage = ({ alliance, size = 'medium' }: { alliance: Alliance; size?: 'small' | 'medium' }) => {
-    const sizePx = size === 'medium' ? 35 : 25;
-    const image = getImageUrl(`mowComponents/resized/${alliance.toLowerCase()}.png`);
-
-    return <img loading={'lazy'} className="pointer-events-none" src={image} height={sizePx} alt={alliance} />;
+const imageMap = {
+    [Alliance.Chaos]: chaos,
+    [Alliance.Imperial]: imperial,
+    [Alliance.Xenos]: xenos,
 };
+
+export const ComponentImage = ({ alliance, size = 'medium' }: { alliance: Alliance; size?: 'small' | 'medium' }) => (
+    <img
+        loading={'lazy'}
+        className="pointer-events-none"
+        src={imageMap[alliance]}
+        height={size === 'medium' ? 35 : 25}
+        alt={alliance}
+    />
+);
