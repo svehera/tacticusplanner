2b3cdffbed9b8fdafea02d46cf63685e5a239846
jtannas <jtannas@gmail.com>
2026-01-15 13:39:15 -0700

    deprecate dynamic image loading functions

--- src/fsd/5-shared/ui/get-image-url.ts
+++ src/fsd/5-shared/ui/get-image-url.ts
@@ -1,3 +1,7 @@
+/**
+ * @deprecated Vite handles asset URLs automatically when they are imported; use direct imports instead.
+ *      Doing these dynamic URLs stops Vite from being able to process them and they end up excluded.
+ */
 export function getImageUrl(image: string): string {
     return new URL(`../../../assets/images/${image}`, import.meta.url).href;
 }
