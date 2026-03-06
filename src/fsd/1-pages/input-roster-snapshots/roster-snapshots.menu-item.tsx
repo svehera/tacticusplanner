ce9aa6e8df794e656846f46676c6260c9d57f929
unrstuart <cpunerd@gmail.com>
2026-03-06 21:40:46 +0100

    Add the ability to import legacy teams since we are turning that page down.

--- src/fsd/1-pages/input-roster-snapshots/roster-snapshots.menu-item.tsx
+++ src/fsd/1-pages/input-roster-snapshots/roster-snapshots.menu-item.tsx
@@ -3,8 +3,4 @@ import GroupsIcon from '@mui/icons-material/Groups';
 // eslint-disable-next-line import-x/no-internal-modules
 import { MenuItem } from '@/models/menu-item';
 
-export const rosterSnapshotsMenuItem = new MenuItem(
-    'Roster Snapshots (BETA)',
-    <GroupsIcon />,
-    '/input/roster-snapshots'
-);
+export const rosterSnapshotsMenuItem = new MenuItem('Roster Snapshots', <GroupsIcon />, '/input/roster-snapshots');
