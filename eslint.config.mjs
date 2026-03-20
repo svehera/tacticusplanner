ba87a950c401c4ee987f356e38451eb7a8071b98
jtannas <jtannas@gmail.com>
2026-03-20 11:49:46 -0600

    revert accidental empty line addition

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -93,7 +93,6 @@ export default defineConfig([
             react: {
                 version: 'detect',
             },
-
             'import/resolver': {
                 typescript: {
                     alwaysTryTypes: true,
